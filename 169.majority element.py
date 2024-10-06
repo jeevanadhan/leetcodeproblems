835d78fe78eb51b1cfcc1c302d94544906488cd8
jeevanadhan <jeevanadhan8014@gmail.com>
2024-10-06 20:41:02 +0530

    Add majority element function
    Implemented a function to find the majority element in a list of integers. This function uses a dictionary to count occurrences and returns the element that appears more than half the time.

---
+++ 169.majority element.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        m={}
+        for i in nums:
+            m[i]=m.get(i,0)+1
+
+            if m[i] > len(nums)/2:
+                return i
